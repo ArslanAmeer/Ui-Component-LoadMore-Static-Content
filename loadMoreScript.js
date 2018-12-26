05ea30ae4a93082ed3aad23590980754bea1e990
ArslanAmeer <arslan.ameer@hotmail.com>
2018-12-27 01:48:26 +0500

    ♻👌 Update: Add Spaces & Indents

--- loadMoreScript.js
+++ loadMoreScript.js
@@ -1,10 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
 
     var divs = document.querySelectorAll("#inLoader");
+
     if (divs.length != 0 && divs.length > 6) {
+
         for (let i = 6; i < divs.length; i++) {
+            // divs[i].classList.add("fadeInUp");
             divs[i].classList.toggle('hiddenContent');
         }
+
     }
 
     const elToLoad = document.getElementById("loadmore");
@@ -12,17 +16,24 @@ document.addEventListener("DOMContentLoaded", function () {
 
         var hiddenDivs = document.querySelectorAll(".hiddenContent");
         const items = hiddenDivs.length;
+
         if (items == 0) {
+
             elToLoad.innerText = 'No More Content';
             elToLoad.removeAttribute('href');
+
         } else if (items != 0 && items > 3) {
+
             for (let i = 0; i < 3; i++) {
                 hiddenDivs[i].classList.toggle('hiddenContent');
             }
+
         } else {
+
             for (let i = 0; i < items; i++) {
                 hiddenDivs[i].classList.toggle('hiddenContent');
             }
+
         }
 
     });
