2453360e8bfacf924d42e327a5c37f4b39a4a30a
ArslanAmeer <arslan.ameer@hotmail.com>
2018-12-27 02:06:40 +0500

    ✨👍 Add: Animation Added

--- loadMoreScript.js
+++ loadMoreScript.js
@@ -5,33 +5,35 @@ document.addEventListener("DOMContentLoaded", function () {
     if (divs.length != 0 && divs.length > 6) {
 
         for (let i = 6; i < divs.length; i++) {
-            // divs[i].classList.add("fadeInUp");
             divs[i].classList.toggle('hiddenContent');
         }
 
     }
 
     const elToLoad = document.getElementById("loadmore");
-    elToLoad.addEventListener("click", () => {
+    elToLoad.addEventListener("click", (e) => {
 
+        e.preventDefault();
         var hiddenDivs = document.querySelectorAll(".hiddenContent");
         const items = hiddenDivs.length;
+        elToLoad.scrollIntoView();
 
         if (items == 0) {
 
             elToLoad.innerText = 'No More Content';
-            elToLoad.removeAttribute('href');
 
         } else if (items != 0 && items > 3) {
 
             for (let i = 0; i < 3; i++) {
                 hiddenDivs[i].classList.toggle('hiddenContent');
+                hiddenDivs[i].classList.add("animated", "fadeInUp");
             }
 
         } else {
 
             for (let i = 0; i < items; i++) {
                 hiddenDivs[i].classList.toggle('hiddenContent');
+                hiddenDivs[i].classList.add("animated", "fadeInUp");
             }
 
         }
